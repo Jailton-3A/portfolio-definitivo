42d2d2be49269d7bac30e5c2eb3b9ad8797a0d39
Jailton-3A <159263821+Jailton-3A@users.noreply.github.com>
2024-05-07 20:32:13 -0300

    alterando textos e imagens

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const nav = document.querySelector(".nav"),
 navList = nav.querySelectorAll("li"),
 totalNavList = navList.length,
-allSection = document.querySelectorAll("section"),
+allSection = document.querySelectorAll(".section"),
 totalSection = allSection.length
 for(let i=0; i<totalNavList; i++){
     const a = navList[i].querySelector("a");
     a.addEventListener("click", function(){
         for(let j=0; j<totalNavList; j++){
+            if(navList[j].querySelector("a").classList.contains("active")){
+                console.log(navList[j].querySelector("a"))
+            }
             navList[j].querySelector("a").classList.remove("ativo");
         }
         this.classList.add("ativo")
